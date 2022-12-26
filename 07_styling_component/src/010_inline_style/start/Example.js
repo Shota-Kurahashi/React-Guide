40bcb43c84121db7893f254ad5cbadadcd5d6299
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-12-26 11:42:23 +0900

    inline-style

--- 07_styling_component/src/010_inline_style/start/Example.js
+++ 07_styling_component/src/010_inline_style/start/Example.js
@@ -1,16 +1,30 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const Example = () => {
-    const [isSelected, setIsSelected] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
 
-    const clickHandler = () => setIsSelected(prev => !prev);
+  const clickHandler = () => setIsSelected((prev) => !prev);
 
-    return (
-        <>
-            <button onClick={clickHandler}>ボタン</button>
-            <div>{isSelected && "クリックされました。"}</div>
-        </>
-    )
+  const style = {
+    width: "120px",
+    height: "60px",
+    display: "block",
+    fontWeight: "bold",
+    borderRadius: "50%",
+    cursor: "pointer",
+    border: "none",
+    margin: "0 auto",
+    background: isSelected ? "pink" : "",
+  };
+
+  return (
+    <>
+      <button onClick={clickHandler} style={style}>
+        ボタン
+      </button>
+      <div>{isSelected && "クリックされました。"}</div>
+    </>
+  );
 };
 
 export default Example;
