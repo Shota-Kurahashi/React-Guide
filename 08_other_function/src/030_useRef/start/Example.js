f5c4333e43ef394ca4f9c1e35108d2a679b2bca3
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-12-26 14:06:44 +0900

    useRef

--- 08_other_function/src/030_useRef/start/Example.js
+++ 08_other_function/src/030_useRef/start/Example.js
@@ -1,14 +1,47 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Case1 = () => {
   const [value, setValue] = useState("");
+  const [isPlay, setIsPlay] = useState(false);
+
+  const inputRef = useRef();
+
+  const movieRef = useRef();
 
   return (
     <div>
       <h3>ユースケース1</h3>
-      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-      <button onClick>
+      <input
+        type="text"
+        ref={inputRef}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={() => inputRef.current.focus()}>
         インプット要素をフォーカスする
+      </button>
+      <video
+        style={{
+          width: "100%",
+        }}
+        ref={movieRef}
+      >
+        <source
+          src="https://www.w3schools.com/html/mov_bbb.mp4"
+          type="video/mp4"
+        />
+      </video>
+      <button
+        onClick={() => {
+          if (isPlay) {
+            movieRef.current.pause();
+          } else {
+            movieRef.current.play();
+          }
+          setIsPlay(!isPlay);
+        }}
+      >
+        {isPlay ? "動画を一時停止する" : "動画を再生する"}
       </button>
     </div>
   );
