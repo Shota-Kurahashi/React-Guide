4303ca283476c247d653b8b0c5365960a39c26ea
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-12-26 12:45:31 +0900

    cahraUI

--- 07_styling_component/src/050_chakra_ui/start/components/Form.js
+++ 07_styling_component/src/050_chakra_ui/start/components/Form.js
@@ -1,9 +1,22 @@
+import { Button, HStack, Input, useToast } from "@chakra-ui/react";
 import { useState } from "react";
 const Form = ({ createTodo }) => {
   const [enteredTodo, setEnteredTodo] = useState("");
 
+  const toasts = useToast();
+
   const addTodo = (e) => {
     e.preventDefault();
+    if (!enteredTodo) {
+      toasts({
+        title: "エラー",
+        status: "error",
+        duration: 2000,
+        isClosable: true,
+        description: "入力してください",
+      });
+      return;
+    }
 
     const newTodo = {
       id: Math.floor(Math.random() * 1e5),
@@ -17,15 +30,33 @@ const Form = ({ createTodo }) => {
   return (
     <div>
       <form onSubmit={addTodo}>
-        <input
-          type="text"
-          value={enteredTodo}
-          onChange={(e) => setEnteredTodo(e.target.value)}
-        />
-        <button>追加</button>
+        <HStack>
+          <Input
+            placeholder="新しいタスク"
+            _placeholder={{ opacity: "0.3", color: "gray.500" }}
+            size="lg"
+            p={3}
+            bgColor="white"
+            variant="flushed"
+            value={enteredTodo}
+            onChange={(e) => setEnteredTodo(e.target.value)}
+          />
+          <Button
+            borderColor={"blue.200"}
+            _hover={{
+              bgColor: "blue.100",
+            }}
+            m={2}
+            borderWidth={"2px"}
+            bgColor="white"
+            type="submit"
+          >
+            追加
+          </Button>
+        </HStack>
       </form>
     </div>
   );
 };
 
-export default Form;
+export { Form };
--- 07_styling_component/src/050_chakra_ui/start/components/List.js
+++ 07_styling_component/src/050_chakra_ui/start/components/List.js
@@ -1,19 +1,46 @@
-const List = ({todos, deleteTodo}) => {
-    const complete = (id) => {
-        deleteTodo(id)
-    }
-    return (
-        <div>
-            {todos.map(todo => {
-                return (
-                    <div key={todo.id}>
-                        <button onClick={() => complete(todo.id)}>完了</button>
-                        <span>{todo.content}</span>
-                    </div>
-                )
-            })}
-        </div>
-    );
-}
+import {
+  VStack,
+  StackDivider,
+  HStack,
+  IconButton,
+  Text,
+} from "@chakra-ui/react";
+import { VscCheck } from "react-icons/vsc";
+
+const List = ({ todos, deleteTodo }) => {
+  const complete = (id) => {
+    deleteTodo(id);
+  };
+  return (
+    <VStack
+      divider={<StackDivider borderColor="gray.200" />}
+      color={{ sm: "red.500", md: "blue.500" }}
+      borderColor="black.100"
+      bgColor="white"
+      borderWidth="1px"
+      borderRadius="3px"
+      alignItems={"start"}
+      p={5}
+      width="80%"
+    >
+      {todos.map((todo) => {
+        return (
+          <HStack key={todo.id} spacing="5">
+            <IconButton
+              isRound
+              bgColor={"cyan.100"}
+              opacity={0.5}
+              icon={<VscCheck />}
+              onClick={() => complete(todo.id)}
+            >
+              完了
+            </IconButton>
+            <Text>{todo.content}</Text>
+          </HStack>
+        );
+      })}
+    </VStack>
+  );
+};
 
 export default List;
