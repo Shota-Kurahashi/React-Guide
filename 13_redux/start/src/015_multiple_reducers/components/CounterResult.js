7cb0d0ca05b507b3da2234425d4d3f3824c324ed
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-12-27 16:05:35 +0900

    combineReducers

--- 13_redux/start/src/015_multiple_reducers/components/CounterResult.js
+++ 13_redux/start/src/015_multiple_reducers/components/CounterResult.js
@@ -1,8 +1,9 @@
 import { useCounter } from "../context/CounterContext";
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
 const CounterResult = () => {
   // const state = useCounter();
-  const state = useSelector(state => state);
+  const state = useSelector((state) => state.counters);
+  console.log(state);
   return <h3>{state}</h3>;
 };
 
--- 13_redux/start/src/015_multiple_reducers/store/index.js
+++ 13_redux/start/src/015_multiple_reducers/store/index.js
@@ -1,17 +1,30 @@
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 
 const initialState = 0;
 const reducer = (state = initialState, { type, step }) => {
-    switch (type) {
-      case "+":
-        return state + step;
-      case "-":
-        return state - step;
-      default:
-        return state;
-    }
-  };
+  switch (type) {
+    case "+":
+      return state + step;
+    case "-":
+      return state - step;
+    default:
+      return state;
+  }
+};
+const reducer2 = (state = initialState, { type, step }) => {
+  switch (type) {
+    case "+":
+      return state + step;
+    case "-":
+      return state - step;
+    default:
+      return state;
+  }
+};
 
-export default createStore(
-    reducer
-);
+const reducers = combineReducers({
+  counters: reducer,
+  counters2: reducer2,
+});
+
+export default createStore(reducers);
