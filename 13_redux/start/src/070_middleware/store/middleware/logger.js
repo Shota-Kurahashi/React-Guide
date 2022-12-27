736753ab3fff2c6ee0b08be10cc95e2b9de033d8
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-12-27 19:04:23 +0900

    section-13 redux 終わり　🚀

---
+++ 13_redux/start/src/070_middleware/store/middleware/logger.js
@@ -0,0 +1,14 @@
+// Redux Middleware
+export const logger = (store) => {
+  return (next) => {
+    return (action) => {
+      // 全てのdispatchで行われる処理
+      // storeはaction前の状態
+      // （store.getState()でステートを取得）
+      console.log(store.getState(), action);
+      next(action);
+      // storeはaction後の状態
+      console.log(store.getState());
+    };
+  };
+};
