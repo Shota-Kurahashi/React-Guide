14eef43722270de6bc57632842813bc231879346
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-12-27 09:28:20 +0900

    practice ruducer

--- 11_hooks_p1/src/025_practice_useReducer/start/Example.js
+++ 11_hooks_p1/src/025_practice_useReducer/start/Example.js
@@ -2,10 +2,27 @@ import { useReducer } from "react";
 
 const CALC_OPTIONS = ["add", "minus", "divide", "multiply"];
 
-const reducer = () => {}
+const reducer = (prev, { type, payload }) => {
+  switch (type) {
+    case "change":
+      return { ...prev, [payload.name]: payload.value };
+    case "add":
+      return { ...prev, result: prev.a + prev.b, type };
+    case "minus":
+      return { ...prev, result: prev.a - prev.b, type };
+    case "divide":
+      return { ...prev, result: prev.a / prev.b, type };
+    case "multiply":
+      return { ...prev, result: prev.a * prev.b, type };
+
+    default:
+      throw new Error("不明なactionです。");
+  }
+};
 
 const Example = () => {
   const initState = {
+    type: "minus",
     a: 1,
     b: 2,
     result: 3,
@@ -14,18 +31,23 @@ const Example = () => {
   const [state, dispatch] = useReducer(reducer, initState);
 
   const calculate = (e) => {
-    
+    dispatch({ type: e.target.value });
   };
 
   const numChangeHandler = (e) => {
-    
-  }
+    dispatch({
+      type: "change",
+      payload: { name: e.target.name, value: +e.target.value },
+    });
+  };
+
+  console.log(state);
 
   return (
     <>
-    <h3>練習問題</h3>
-    <p>useReducerを使って完成コードと同じ機能を作成してください。</p>
-      {/* <div>
+      <h3>練習問題</h3>
+      <p>useReducerを使って完成コードと同じ機能を作成してください。</p>
+      <div>
         a:
         <input
           type="number"
@@ -44,8 +66,13 @@ const Example = () => {
         />
       </div>
       <select value={state.type} onChange={calculate}>
+        {CALC_OPTIONS.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
       </select>
-      <h1>結果：{state.result}</h1> */}
+      <h1>結果：{state.result}</h1>
     </>
   );
 };
