c961457d01055f94bc581ddcf4407e7ef214ca2e
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-12-27 19:24:41 +0900

    object.is

--- 15_performance/src/012_strictmode/start/Example.js
+++ 15_performance/src/012_strictmode/start/Example.js
@@ -3,25 +3,25 @@ import "./Example.css";
 
 const Example = () => {
   console.log("render");
-  
+
   const [countA, setCountA] = useState({
-    val: 0
+    val: 0,
   });
 
-  const obj1 = {val:0};
-  const obj3 = {val:0};
-  const obj2 = { ...obj1};
-  const isSame = Object.is("",false);
+  const obj1 = { val: 0 };
+  const obj3 = { val: 0 };
+  const obj2 = { ...obj1 };
+  const isSame = Object.is("", false);
   const isSameEqual = "" == false;
   console.log(isSameEqual);
-  
+
   return (
     <div className="parent">
       <div>
         <h3>再レンダリング？</h3>
         <button
           onClick={() => {
-            setCountA(prev => {
+            setCountA((prev) => {
               const newState = { ...prev };
               // prev.val = 1;
               return newState;
