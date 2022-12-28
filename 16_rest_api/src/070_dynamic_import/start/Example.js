862b92aea8bfb0ddb72b3d7576054ae6608b6ef1
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-12-28 11:38:20 +0900

    dynammic import

--- 16_rest_api/src/070_dynamic_import/start/Example.js
+++ 16_rest_api/src/070_dynamic_import/start/Example.js
@@ -1,5 +1,9 @@
+import { add } from "./add";
+
 const Example = () => {
-  
+  import("./add").then((module) => console.log(module.add));
+  console.log(add(1, 2));
+  return <></>;
 };
 
 export default Example;
